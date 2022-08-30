4da1d5c14bb69b5e01bd48e90ba00951576eaff5
HernanySimao <hernanysimao123@gmail.com>
2022-08-30 13:58:52 -0300

    BG update

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,8 @@ export default {};
 .bg-main {
   background: url('@/assets/img.jpg') no-repeat;
   background-attachment: fixed;
-  /* background: #0c142a;
-  width: 100p0x; */
 }
+
+ 
+
 </style>
